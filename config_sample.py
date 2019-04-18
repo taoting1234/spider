15e61092f37f5b78fe427e34e9d21d7f53de0d3f
taoting <335354289@qq.com>
2019-04-18 20:56:58 +0800

    修改单词

--- config_sample.py
+++ config_sample.py
@@ -1,4 +1,4 @@
-# please copy this demo and rename to config.py
+# please copy this sample and rename to config.py
 default_ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) ' \
              'AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
 proxy_url = ""
