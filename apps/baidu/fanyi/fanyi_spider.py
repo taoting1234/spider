a4593f9b082ea39080238ef6098cee963daa964d
taoting <335354289@qq.com>
2019-04-18 17:48:00 +0800

    添加文档

--- apps/baidu/fanyi/fanyi_spider.py
+++ apps/baidu/fanyi/fanyi_spider.py
@@ -3,7 +3,15 @@ from apps.baidu.fanyi.fanyi_http import FanyiHttp
 
 
 class FanyiSpider:
-    def translate(self, keywords: str, from_: str = 'en', to: str = 'zh') -> str:
+    @staticmethod
+    def translate(keywords: str, from_: str = 'en', to: str = 'zh') -> str:
+        """
+        翻译
+        :param keywords: 需要翻译的单词或句子
+        :param from_: 从什么语言翻译
+        :param to: 翻译到什么语言
+        :return: 翻译的结果
+        """
         url = "https://fanyi.baidu.com/basetrans"
         token = "98945f0628cf2a4b552845b3417270cd"
         data = {
