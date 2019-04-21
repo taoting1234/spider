ee5f53054c0725b193168c1b96f9fe96ba8e6297
taoting <335354289@qq.com>
2019-04-21 22:30:47 +0800

    update zf

--- apps/zf/zf_http.py
+++ apps/zf/zf_http.py
@@ -3,6 +3,6 @@ from libs.http import Http
 
 class ZfHttp(Http):
     def __init__(self):
-        super().__init__(random_ua=False, proxy=False)
+        super().__init__()
         self.sess.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
         self.sess.headers['Content-Type'] = 'application/x-www-form-urlencoded'
